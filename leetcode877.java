26acc0f40ad2b46589aad0dcb9d789795c843a99
LeetCode666 <73152669+LeetCode666@users.noreply.github.com>
2020-10-29 23:15:46 -0400

    Add files via upload

---
+++ leetcode877.java
@@ -0,0 +1,17 @@
+package exam;
+
+public class leetcode877 {
+	public boolean stoneGame(int[] piles) {
+        int len = piles.length;
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = j - 1; i >= 0; i--) {
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][len - 1] > 0;
+    }
+}
